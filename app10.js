f74029f7b4affa7e3a072b095da2e6d0e67b6df2
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 19:05:48 +0530

    Add files via upload

---
+++ app10.js
@@ -0,0 +1,13 @@
+let marks = 5;
+if(marks >= 33) {
+    console.log("Pass");
+    if(marks >= 80) {
+        console.log("Grade : O");
+    }
+    else {
+        console.log("Grade : A");
+    }
+}
+else {
+    console.log("Better luck next time!");
+}
